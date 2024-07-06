01a71f1dea2e1c995e697625b5612f4bfec2bc81
novasqw <ykorkmazoglu321@gmail.com>
2024-07-06 23:54:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ function onayla() {
     if (isim.trim() !== '') {
         document.getElementById('girisKutusu').style.display = 'none';
         document.getElementById('chatAlanı').style.display = 'block';
+        localStorage.setItem('isim', isim); // Kullanıcı adını local storage'a kaydet
     }
 }
 
 function mesajGonder() {
     var mesaj = document.getElementById('mesajInput').value;
     if (mesaj.trim() !== '') {
+        var isim = localStorage.getItem('isim');
         var mesajlarDiv = document.getElementById('mesajlar');
         mesajlarDiv.innerHTML += '<p><strong>' + isim + ':</strong> ' + mesaj + '</p>';
         document.getElementById('mesajInput').value = '';
         mesajlarDiv.scrollTop = mesajlarDiv.scrollHeight;
     }
 }
-
-var isim = ''; // Kullanıcının ismi
